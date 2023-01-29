a26ad6e60aeb4a4475eebc6cf98dd16e9505e6a1
Jeet Patel <redhinesward@gmail.com>
2023-01-29 16:40:19 -0600

    <This program calcualtes the trip of each leg and total trip>

---
+++ homework02/calculating_trip.py
@@ -0,0 +1,46 @@
+import math
+import json 
+mars_radius = 3389.5    # km
+
+def calc_gcd(latitude_1: float, longitude_1: float, latitude_2: float, longitude_2: float) -> float:
+        lat1, lon1, lat2, lon2 = map( math.radians, [latitude_1, longitude_1, latitude_2, longitude_2] )
+        d_sigma = math.acos( math.sin(lat1) * math.sin(lat2) + math.cos(lat1) * math.cos(lat2) * math.cos(abs(lon1-lon2)))
+        return ( mars_radius * d_sigma )
+
+
+with open('sites.json') as f: 
+    data = json.load(f) 
+
+
+robot_start_lat = 16.0
+robot_start_lon = 82.0
+robot_speed = 10
+total_time = 0
+total_distance = 0
+i = 0
+count = 0 
+
+for i in range(5):  
+    if i == 0: 
+        current_lat, current_lon = robot_start_lat, robot_start_lon
+    else: 
+        current_lat, current_lon = data['sites'][i-1]['latitude'], data['sites'][i-1]['longitude']
+    next_lat, next_lon =  data['sites'][i]['latitude'], data['sites'][i]['longitude'] 
+    distance = calc_gcd(current_lat,current_lon,next_lat,next_lon) 
+
+    travel_time = distance/robot_speed 
+    
+    if data['sites'][i]['composition'] == 'stony': 
+        sample_time = 1
+    elif data['sites'][i]['composition'] == 'iron':
+        sample_time = 2
+    elif data['sites'][i]['composition'] == 'stony-iron':
+        sample_time = 3 
+    total_time += travel_time + sample_time 
+    count += 1 
+    print("leg = ", count,",", f"time to travel = {travel_time:.2f} hr,", "time to sample = ", sample_time, " hr")  
+    
+
+
+print("number of legs = ", count, ",", f"total time elapsed = {total_time:.2f} hr")
+
